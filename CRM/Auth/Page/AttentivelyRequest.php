8778393c7f4245b7e69389981c6bddec10ff64d3
Edsel <edsel.lopez@jmaconsulting.biz>
2015-02-19 13:14:07 +0530

    AI-23 Bug fixes and code improvements

--- CRM/Auth/Page/AttentivelyRequest.php
+++ CRM/Auth/Page/AttentivelyRequest.php
@@ -5,7 +5,7 @@ require_once 'CRM/Core/Page.php';
 class CRM_Auth_Page_AttentivelyRequest extends CRM_Core_Page {
   function run() {
     $redirect = CRM_Utils_Array::value('redirect', $_GET);
-    $redirectUri = 'http://attentive.ly.jmaconsulting.biz/civicrm/attentively/callback';
+    $redirectUri = 'http://attentive.ly.jmaconsulting.biz/civicrm/attentively/authcallback';
     $url = CRM_Auth_BAO_AttentivelyAuth::checkEnvironment();
     $url = $url . 'connect?client_id='.CLIENT_ID.'&redirect_uri='.$redirectUri;
     CRM_Core_Session::singleton()->set('redirectDomain', $redirect);
